b592861d7b3ac003ef79410337b6c8e02b74874d
peco <ytpecopeco@gmail.com>
2019-08-01 09:36:37 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,23 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: require('@/views/Home').default
     },
     {
       path: '/about',
       name: 'about',
-      component: About
+      component: require('@/views/About').default
+    },
+    {
+      path: '*',
+      redirect: '/'
     }
   ]
 })
