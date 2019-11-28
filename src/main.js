d07d8bd397cc706551424c5d40622728587eabb6
pecopeco <yangpeco@qq.com>
2019-11-28 17:14:31 +0800

    update

--- src/main.js
+++ src/main.js
@@ -44,7 +44,7 @@ function request (url, form = {}, type) {
   }).then((res) => {
     if (type === 'delete' || res.status === 204) {
       return res.text()
-    } else if (res.data.state === 'T') {
+    } else if (res.status === 200) {
       return res.data
     } else {
       Toast(JSON.parse(res.data).error.msg)
