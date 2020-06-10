f2f2209301ed4d715d279edc054ccce75f9822b8
pecopeco <yangpeco@qq.com>
2020-06-10 09:59:00 +0800

    修复底栏切换文字抖动bug

--- src/components/tabBar.vue
+++ src/components/tabBar.vue
@@ -1,16 +1,16 @@
 <template lang="pug">
-  .tab-bar
+  .tab-bar(v-if="$route.name === 'home' || $route.name === 'about' || $route.name === 'my'")
     .item(:class="{'checked': checkTab === 'home'}" @click="check('home')")
-      img(v-if="checkTab === 'home'" src="@/assets/home-check.png")
-      img(v-else src="@/assets/home.png")
+      .img-wrap
+        img(:src="checkTab === 'home' ? require('@/assets/home-check.png') : require('@/assets/home.png')")
       span 首页
     .item(:class="{'checked': checkTab === 'about'}" @click="check('about')")
-      img(v-if="checkTab === 'about'" src="@/assets/about-check.png")
-      img(v-else src="@/assets/about.png")
+      .img-wrap
+        img(:src="checkTab === 'about' ? require('@/assets/about-check.png') : require('@/assets/about.png')")
       span 关于
     .item(:class="{'checked': checkTab === 'my'}" @click="check('my')")
-      img(v-if="checkTab === 'my'" src="@/assets/my-check.png")
-      img(v-else src="@/assets/my.png")
+      .img-wrap
+        img(:src="checkTab === 'my' ? require('@/assets/my-check.png') : require('@/assets/my.png')")
       span 我的
 </template>
 
@@ -56,14 +56,12 @@ export default {
   }
   .item {
     flex 1
-    display flex
-    flex-direction column
-    align-items center
-    justify-content space-around
+    padding-top .02rem
     color fontGray
-    img {
-      width .18rem
-      height .18rem
+    .img-wrap {
+      width .2rem
+      height .2rem
+      margin 0 auto .02rem
     }
     span {
       padding-top .02rem
